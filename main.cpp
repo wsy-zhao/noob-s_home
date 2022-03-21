ab0e2983f1bc3ba017a56a0346c026259def387c
wsy-zhao <zwsy@gmail.com>
2022-03-21 21:29:42 +0800

    cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,15 @@
 using namespace std;
 
 int main(){
-    Solution s;
-    TreeNode t2(2), t4(4), t7(7);
-    TreeNode t3(3, &t2, &t4), t6(6, nullptr, &t7);
-    TreeNode t5(5, &t3, &t6);
-    bool ans = s.findTarget(&t5, 9);
-    cout << ans << endl;
+    int x = 10;
+    while (x --> 0){
+        cout << x << " ";
+    }
+    // Solution s;
+    // TreeNode t2(2), t4(4), t7(7);
+    // TreeNode t3(3, &t2, &t4), t6(6, nullptr, &t7);
+    // TreeNode t5(5, &t3, &t6);
+    // bool ans = s.findTarget(&t5, 9);
+    // cout << ans << endl;
     return 0;
 }
